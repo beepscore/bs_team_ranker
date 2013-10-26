bd23cb8af1a2df813289e5ac0f69a7bc8a1027c9
beepscore <support@beepscore.com>
2013-10-26 16:03:28 -0700

    In game_team add methods name_from_team_string and score_from_team_string. In game_team_test add corresponding tests.

--- lib/game_team.rb
+++ lib/game_team.rb
@@ -3,12 +3,36 @@
 # GameTeam is a lightweight team for use by Game
 class GameTeam
 
-  attr_reader   :name
-  attr_accessor :score
+  attr_reader :name
+  attr_reader :score
 
-  def initialize(name)
-    @name = name
-    @score = 0
+  def initialize(a_team_string)
+    unless a_team_string.nil?
+      @name = name_from_team_string(a_team_string)
+      @score = score_from_team_string(a_team_string)
+    end
+  end
+
+  def name_from_team_string(a_team_string)
+    name = nil
+    unless a_team_string.nil?
+      # delete score. match one or more whitespace followed by one or more digits followed by end of line
+      name = a_team_string.gsub(/\s+\d+$/, '')
+    end
+    name
+  end
+
+  def score_from_team_string(a_team_string)
+    score = nil
+    unless a_team_string.nil?
+      # match one or more digits followed by end of line
+      re = /\d+$/
+      match_data = a_team_string.match re
+      unless match_data.nil?
+        score = match_data[0].to_i
+      end
+    end
+    score
   end
 
 end
--- test/game_team_test.rb
+++ test/game_team_test.rb
@@ -6,13 +6,29 @@ require_relative '../lib/game_team'
 class GameTeamTest < MiniTest::Unit::TestCase
 
   def test_new_sets_name
-    a_game_team = GameTeam.new('Ravens')
+    a_game_team = GameTeam.new('Ravens 12')
     assert_equal('Ravens', a_game_team.name)
   end
 
   def test_new_sets_score
-    a_game_team = GameTeam.new('Alphas')
-    assert_equal(0, a_game_team.score)
+    a_game_team = GameTeam.new('Ravens 12')
+    assert_equal(12, a_game_team.score)
+  end
+
+  def test_name_from_team_string
+    a_game_team = GameTeam.new(nil)
+
+    assert_equal('Tarantulas', a_game_team.name_from_team_string('Tarantulas 3'))
+    assert_equal('FC Awesome', a_game_team.name_from_team_string('FC Awesome 0'))
+    assert_equal('áƏĭö', a_game_team.name_from_team_string('áƏĭö 14'))
+  end
+
+  def test_score_from_team_string
+    a_game_team = GameTeam.new(nil)
+
+    assert_equal(3, a_game_team.score_from_team_string('Tarantulas 3'))
+    assert_equal(0, a_game_team.score_from_team_string('FC Awesome 0'))
+    assert_equal(14, a_game_team.score_from_team_string('áƏĭö 14'))
   end
 
 end
