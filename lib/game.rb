4c601602302616cb9ad14298f0d5a55fd1fb0cc6
beepscore <support@beepscore.com>
2013-10-25 19:39:40 -0700

    In game.rb init, strip whitespace. In name() use regex to delete score. In game_test.rb test_new_sets_teams add assertions.

--- lib/game.rb
+++ lib/game.rb
@@ -2,22 +2,23 @@
 
 class Game
 
+  # game has 2 teams, so don't bother to make a teams collection
   attr_reader :team_a
   attr_reader :team_b
   attr_reader :team_a_score
   attr_reader :team_b_score
 
   def initialize(game_string)
-    team_a_string = game_string.split(',')[0]
-    team_b_string = game_string.split(',')[1]
+    team_a_string = game_string.split(',')[0].strip()
+    team_b_string = game_string.split(',')[1].strip()
 
     @team_a = name(team_a_string)
+    @team_b = name(team_b_string)
   end
 
   def name(team_string)
-    # simplest implementation that can pass test
-    # TODO use regex to parse string?
-    'Tarantulas'
+    # delete score. match one or more whitespace followed by one or more digits followed by end of line
+    team_string.gsub(/\s+\d+$/, '')
   end
 
 end
--- test/game_test.rb
+++ test/game_test.rb
@@ -8,6 +8,15 @@ class GameTest < MiniTest::Unit::TestCase
   def test_new_sets_teams
     a_game = Game.new('Tarantulas 3, Snakes 1')
     assert_equal('Tarantulas', a_game.team_a)
+    assert_equal('Snakes', a_game.team_b)
+
+    a_game = Game.new('Tarantulas 1, FC Awesome 0')
+    assert_equal('Tarantulas', a_game.team_a)
+    assert_equal('FC Awesome', a_game.team_b)
+
+    a_game = Game.new('áƏĭö 14, ƩƿƔƸȢ 268')
+    assert_equal('áƏĭö', a_game.team_a)
+    assert_equal('ƩƿƔƸȢ', a_game.team_b)
   end
 
 end
