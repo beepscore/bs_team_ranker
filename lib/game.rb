428e04c9b06ef75c3b97011b1392a4e4c1d7ee36
beepscore <support@beepscore.com>
2013-10-25 21:20:52 -0700

    In game.rb add checks for nil, implement score. In game_test.rb add test_name and test_score

--- lib/game.rb
+++ lib/game.rb
@@ -9,16 +9,38 @@ class Game
   attr_reader :team_b_score
 
   def initialize(game_string)
-    team_a_string = game_string.split(',')[0].strip()
-    team_b_string = game_string.split(',')[1].strip()
+    unless game_string.nil?
+      team_a_string = game_string.split(',')[0].strip()
+      team_b_string = game_string.split(',')[1].strip()
 
-    @team_a = name(team_a_string)
-    @team_b = name(team_b_string)
+      @team_a = name(team_a_string)
+      @team_b = name(team_b_string)
+
+      @team_a_score = score(team_a_string)
+      @team_b_score = score(team_b_string)
+    end
   end
 
   def name(team_string)
-    # delete score. match one or more whitespace followed by one or more digits followed by end of line
-    team_string.gsub(/\s+\d+$/, '')
+    name = nil
+    unless team_string.nil?
+      # delete score. match one or more whitespace followed by one or more digits followed by end of line
+      name = team_string.gsub(/\s+\d+$/, '')
+    end
+    name
+  end
+
+  def score(team_string)
+    score = nil
+    unless team_string.nil?
+      # match one or more digits followed by end of line
+      re = /\d+$/
+      match_data = team_string.match re
+      unless match_data.nil?
+        score = match_data[0]
+      end
+    end
+    score
   end
 
 end
--- test/game_test.rb
+++ test/game_test.rb
@@ -5,6 +5,7 @@ require_relative '../lib/game'
 
 class GameTest < MiniTest::Unit::TestCase
 
+  # this test scope involves multiple methods, more than a single unit
   def test_new_sets_teams
     a_game = Game.new('Tarantulas 3, Snakes 1')
     assert_equal('Tarantulas', a_game.team_a)
@@ -17,6 +18,22 @@ class GameTest < MiniTest::Unit::TestCase
     a_game = Game.new('áƏĭö 14, ƩƿƔƸȢ 268')
     assert_equal('áƏĭö', a_game.team_a)
     assert_equal('ƩƿƔƸȢ', a_game.team_b)
+  end
+
+  def test_name
+    a_game = Game.new(nil)
+
+    assert_equal('Tarantulas', a_game.name('Tarantulas 3'))
+    assert_equal('FC Awesome', a_game.name('FC Awesome 0'))
+    assert_equal('áƏĭö', a_game.name('áƏĭö 14'))
+  end
+
+  def test_score
+    a_game = Game.new(nil)
+
+    assert_equal('3', a_game.score('Tarantulas 3'))
+    assert_equal('0', a_game.score('FC Awesome 0'))
+    assert_equal('14', a_game.score('áƏĭö 14'))
   end
 
 end
