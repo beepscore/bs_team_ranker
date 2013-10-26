3c88a5fa0819fcb4aef609c2bf2c75c583c91d7f
beepscore <support@beepscore.com>
2013-10-26 00:24:35 -0700

    In team_test.rb add test_new_sets_won_tied_lost and test_points. In team.rb add attributes and method to make tests pass.

--- lib/team.rb
+++ lib/team.rb
@@ -2,11 +2,19 @@
 
 class Team
 
-  attr_reader :name
+  attr_reader   :name
+  # number of games won, tied, lost
+  attr_accessor :won, :tied, :lost
 
   def initialize(name)
     @name = name
+    @won = 0
+    @tied = 0
+    @lost = 0
+  end
 
+  def points
+    (3 * won) + tied
   end
 
 end
--- test/team_test.rb
+++ test/team_test.rb
@@ -10,4 +10,26 @@ class TeamTest < MiniTest::Unit::TestCase
     assert_equal('Ravens', a_team.name)
   end
 
+  def test_new_sets_won_tied_lost
+    a_team = Team.new('Alphas')
+    assert_equal(0, a_team.won)
+    assert_equal(0, a_team.tied)
+    assert_equal(0, a_team.lost)
+  end
+
+  def test_points
+    a_team = Team.new('Alphas')
+    assert_equal(0, a_team.points)
+
+    a_team.won = 1
+    a_team.tied = 2
+    a_team.lost = 4
+    assert_equal(5, a_team.points)
+
+    a_team.won = 19
+    a_team.tied = 13
+    a_team.lost = 456
+    assert_equal(70, a_team.points)
+  end
+
 end
