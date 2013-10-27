b9170675d45d38e9e8431ef1c91b8508e96bd451
beepscore <support@beepscore.com>
2013-10-27 11:26:02 -0700

    In league_controller and tests delete duplicate code and use bs_file_accessor. In league_controller rename attribute file_string to games_string

--- lib/league_controller.rb
+++ lib/league_controller.rb
@@ -1,46 +1,18 @@
 #!/usr/bin/env ruby
 
+require_relative 'bs_file_accessor'
 require_relative 'game'
 
 class LeagueController
 
-  attr_reader :file_encoding
-  attr_reader :file_name
-  attr_reader :file_string
+  attr_reader :games_string
   attr_reader :games
   attr_accessor :teams
 
   def initialize(file_name, external_encoding)
-    @file_name = file_name
 
-    # ruby 1.9.3 and ruby 2.0 default_external encoding is UTF-8
-    # You can see this from irb
-    # $ irb
-    # irb(main):001:0> Encoding.default_external
-    # => <Encoding:UTF-8>
-
-    # https://www.ruby-lang.org/en/news/2013/02/24/ruby-2-0-0-p0-is-released/
-    # default internal_encoding is nil. set it.
-    internal_encoding = "utf-8"
-    if (external_encoding == internal_encoding)
-      # avoid ruby warning:
-      # Ignoring internal encoding utf-8: it is identical to external encoding utf-8
-      read_access_and_encoding = "r"
-    else
-      read_access_and_encoding = "r:#{external_encoding}:#{internal_encoding}"
-    end
-
-    # at end of block, file will be closed automatically
-    File.open(@file_name, read_access_and_encoding) do |file|
-
-      @file_encoding = file.external_encoding
-
-      @file_string = ""
-      file.each_line do |line|
-        @file_string += line
-      end
-
-    end
+    file_accessor = BSFileAccessor.new(file_name, external_encoding)
+    @games_string = file_accessor.file_string
 
     @games = []
     @teams = []
--- test/league_controller_test.rb
+++ test/league_controller_test.rb
@@ -6,69 +6,6 @@ require_relative '../lib/league_controller'
 
 class LeagueControllerTest < MiniTest::Unit::TestCase
 
-  def test_new_sets_file_encoding
-    # Terminal file command shows
-    # $ file sample-input.txt
-    # sample-input.txt: ASCII text
-    # $ file -I sample-input.txt
-    # sample-input.txt: text/plain; charset=us-ascii
-
-    # sample-input.txt team names don't have accented characters, but don't assume they can't.
-    # In other words, don't assume file encoding will always be ASCII instead of UTF-8.
-    # On Mac I used character viewer to drag accented characters into new file in MacVim
-    # $ file sample-input-utf8.txt
-    # sample-input-utf8.txt: UTF-8 Unicode text
-
-    a_league_controller = LeagueController.new('./sample-input.txt', 'ascii')
-    actual_result = a_league_controller.file_encoding
-    expected_result = Encoding.find('US-ASCII')
-    assert_equal(expected_result, actual_result)
-
-    a_league_controller = LeagueController.new('./sample-input.txt', 'utf-8')
-    actual_result = a_league_controller.file_encoding
-    expected_result = Encoding.find('UTF-8')
-    assert_equal(expected_result, actual_result)
-
-    # this throws error
-    # Encoding::InvalidByteSequenceError: "\xC3" on US-ASCII
-    # a_league_controller = LeagueController.new('./sample-input-utf8.txt', 'ascii')
-
-    a_league_controller = LeagueController.new('./sample-input-utf8.txt', 'utf-8')
-    actual_result = a_league_controller.file_encoding
-    expected_result = Encoding.find('UTF-8')
-    assert_equal(expected_result, actual_result)
-  end
-
-  def test_new_sets_file_string
-
-    a_league_controller = LeagueController.new('./sample-input.txt', 'utf-8')
-    actual_result = a_league_controller.file_string
-    puts ''
-    puts 'file_string:' + actual_result
-    expected_result = <<END
-Lions 3, Snakes 3
-Tarantulas 1, FC Awesome 0
-Lions 1, FC Awesome 1
-Tarantulas 3, Snakes 1
-Lions 4, Grouches 0
-END
-    assert_equal(expected_result, actual_result)
-
-    a_league_controller = LeagueController.new('./sample-input-utf8.txt', 'utf-8')
-    actual_result = a_league_controller.file_string
-    puts ''
-    puts 'file_string:' + actual_result
-    expected_result = <<END
-Lions 3, Snakes 3
-Tarantulas 1, FC Awesome 0
-Lions 1, FC Awesome 1
-Tarantulas 3, Snakes 1
-áƏĭö 14, ƩƿƔƸȢ 268
-Lions 4, Grouches 0
-END
-    assert_equal(expected_result, actual_result)
-  end
-
   def test_new_sets_games
     a_league_controller = LeagueController.new('./sample-input.txt', 'utf-8')
     assert_equal([], a_league_controller.games)
@@ -81,7 +18,7 @@ END
 
   def test_add_games
     a_league_controller = LeagueController.new('./sample-input.txt', 'utf-8')
-    a_league_controller.add_games(a_league_controller.games, a_league_controller.file_string)
+    a_league_controller.add_games(a_league_controller.games, a_league_controller.games_string)
     assert_equal(5, a_league_controller.games.length)
 
     test_game = a_league_controller.games[0]
@@ -89,7 +26,7 @@ END
     assert_equal(3, test_game.game_teams[0].score)
 
     a_league_controller = LeagueController.new('./sample-input-utf8.txt', 'utf-8')
-    a_league_controller.add_games(a_league_controller.games, a_league_controller.file_string)
+    a_league_controller.add_games(a_league_controller.games, a_league_controller.games_string)
     assert_equal(6, a_league_controller.games.length)
 
     test_game = a_league_controller.games[1]
@@ -122,7 +59,7 @@ END
   def test_update_teams
     a_league_controller = LeagueController.new('./sample-input.txt', 'utf-8')
     a_league_controller.teams = []
-    a_league_controller.add_games(a_league_controller.games, a_league_controller.file_string)
+    a_league_controller.add_games(a_league_controller.games, a_league_controller.games_string)
 
     test_game = a_league_controller.games[0]
     assert(!a_league_controller.team_name_in_teams?(a_league_controller.teams, 'Lions'))
